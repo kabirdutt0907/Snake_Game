e6797236bd951b6c017edbeb346437453495dd58
kabirdutt0907 <NANAK@#dutt09>
2021-09-21 12:52:20 +0530

    High Score Feature added .

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from scoreboard import ScoreBoard
 import time
 
 screen = Screen()
-screen.setup(width=600 , height=600)
+screen.setup(width=600, height=600)
 screen.bgcolor("Black")
 screen.title("Nagin Dance")
 screen.tracer(0)
@@ -16,11 +16,10 @@ scorenboard = ScoreBoard()
 
 screen.listen()
 
-screen.onkey(snake.up , "Up")
-screen.onkey(snake.down , "Down")
-screen.onkey(snake.left , "Left")
-screen.onkey(snake.right ,"Right")
-
+screen.onkey(snake.up, "Up")
+screen.onkey(snake.down, "Down")
+screen.onkey(snake.left, "Left")
+screen.onkey(snake.right, "Right")
 
 game_is_on = True
 
@@ -35,20 +34,14 @@ while game_is_on:
         scorenboard.increase_score()
 
     if snake.head.xcor() > 285 or snake.head.xcor() < -285 or snake.head.ycor() > 285 or snake.head.ycor() < -285:
-        scorenboard.Game_Over()
-        game_is_on = False
-
+        scorenboard.reset()
+        snake.reset_snake()
 
     for segments in snake.segments:
         if segments == snake.head:
             pass
         elif snake.head.distance(segments) < 10:
-            game_is_on = False
-            scorenboard.Game_Over1()
-
-
-
-
-
+            scorenboard.reset()
+            snake.reset_snake()
 
 screen.exitonclick()
--- scoreboard.py
+++ scoreboard.py
@@ -5,16 +5,28 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         self.score = 0
+        self.highscore = 0
         self.color("white")
         self.penup()
         self.goto(0, 268)
-        self.write(f"Score: {self.score}" ,align="center" , font=("Courier" , 24 , "normal"))
+        self.write(f"Score: {self.score} | High Score : {self.highscore}" ,align="center" , font=("Courier" , 24 , "normal"))
         self.hideturtle()
+
+    def update_scoreboard(self):
+        self.clear()
+        self.write(f"Score: {self.score} | High Score : {self.highscore}", align="center", font=("Courier", 24, "normal"))
+
+
+    def reset(self):
+        if self.score > self.highscore:
+            self.highscore = self.score
+        self.score = 0
+        self.update_scoreboard()
 
     def increase_score(self):
         self.score += 1
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Courier", 24, "normal"))
+        self.update_scoreboard()
+
 
     def Game_Over(self):
         self.goto(0 , 0)
